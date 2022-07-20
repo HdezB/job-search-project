344694fd5a8f295c2bdfab94a19fe0a393566fb8
DukeHdez <briayan27@outlook.com>
2022-07-20 16:06:35 -0400

    trying to deploy again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const apiRoutes = require('./routes/api')
-
+const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
